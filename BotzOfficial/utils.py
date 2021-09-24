4c70ad15f58d55af31754796c35678ef503d4a94
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-09-24 21:15:00 +0530

    Update utils.py

--- BotzOfficial/utils.py
+++ BotzOfficial/utils.py
@@ -8,11 +8,11 @@ import importlib
 from pathlib import Path
 
 def load_plugins(plugin_name):
-    path = Path(f"TelethonBot/plugins/{plugin_name}.py")
-    name = "TelethonBot.plugins.{}".format(plugin_name)
+    path = Path(f"BotzOfficial/plugins/{plugin_name}.py")
+    name = "BotzOfficial.plugins.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["TelethonBot.plugins." + plugin_name] = load
-    print("TelethonBot has Imported " + plugin_name)
+    sys.modules["BotzOfficial.plugins." + plugin_name] = load
+    print("BotzOfficial has Imported " + plugin_name)
