4fefc969de3d99ca2d97e091bbc0cff3c490bb2c
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-09-24 20:52:06 +0530

    Update __init__.py

--- BotzOfficial/__init__.py
+++ BotzOfficial/__init__.py
@@ -12,8 +12,11 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # Basics
-APP_ID = config("APP_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
+#APP_ID = config("APP_ID", default=None, cast=int)
+#API_HASH = config("API_HASH", default=None)
+#BOT_TOKEN = config("BOT_TOKEN", default=None)
+APP_ID = '7515583'
+API_HASH = 'aaf08fb05d141da368c024c00afcf3f1'
+BOT_TOKEN = '1970566165:AAHgCfI4u02i5ODpEntyI9DeFG-70CqPwys'
 
 BotzOfficial = TelegramClient('BotzOfficial', APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 
